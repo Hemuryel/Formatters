a1ffd43bb80c05e693fed084eb0ff9cafc6ab807
Hemuryel <hyl.silva@gmail.com>
2022-11-16 16:44:13 -0300

    Alterações.

---
+++ NumerosPorExtenso/Program.cs
@@ -0,0 +1,19 @@
+﻿using Caelum.Stella.CSharp.Inwords;
+using System.Diagnostics;
+
+namespace ConsoleAppNumerosPorExtenso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 1532987.89;
+            string extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            // adicionar reais/real, centavos ao final
+            string extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+        }
+    }
+}
